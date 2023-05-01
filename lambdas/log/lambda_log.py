89519b251c5c0e04e3d175d5845997cf76ac6bb6
DanielR-spec <daniel_reyes@javeriana.edu.co>
2023-04-30 21:41:38 -0500

    fix lambda

--- lambdas/log/lambda_log.py
+++ lambdas/log/lambda_log.py
@@ -1,9 +1,4 @@
-import json
-
 def lambda_handler(event, context):
     message = "Hello, world!"
     messageb = "this is another message"
-    return {
-        'statusCode': 200,
-        'body': json.dumps(message+messageb)
-    }
+    return message
