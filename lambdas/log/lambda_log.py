d0244d772e9dbfb9b6d603cd3864ea7104bba2c1
DanielR-spec <daniel_reyes@javeriana.edu.co>
2023-04-30 20:19:54 -0500

    test mofidy code deploy

--- lambdas/log/lambda_log.py
+++ lambdas/log/lambda_log.py
@@ -3,7 +3,8 @@ import pandas as pd
 import json
 def lambda_handler(event, context):
     message = "Hello, world!"
+    messageb = "this is another message"
     return {
         'statusCode': 200,
-        'body': json.dumps(message)
+        'body': json.dumps(message+messageb)
     }
