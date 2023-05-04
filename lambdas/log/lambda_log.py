b9099460a679ff77fe93c6192f53943965d5fba9
DanielR-spec <daniel_reyes@javeriana.edu.co>
2023-05-04 12:05:54 -0500

    v41

--- lambdas/log/lambda_log.py
+++ lambdas/log/lambda_log.py
@@ -1,8 +1,21 @@
 #Import library
 
-import pymysql.cursors
+import psycopg2
 
-def lambda_handler(event, contect):
+HOST = 'motivy-redshift-cluster.cxrt7addrmk7.us-east-1.redshift.amazonaws.com'
+USER = 'rdsamin'
+PASSWORD = 'A0so%33r7Jf6'
+PORT = 5439
+DBNAME = 'dev'
+
+def get_redshift_con(password=PASSWORD,
+                     user=USER,
+                     host=HOST,
+                     port=PORT,
+                     dbname=DBNAME):
+    return psycopg2.connect(dbname=dbname,host=host,port=port,user=user,password=password)
+
+def lambda_handler(event, context):
     # TODO implement
     '''
 	De next steps in order to excecute this lambda is to 
@@ -12,18 +25,32 @@ def lambda_handler(event, contect):
     '''
 
     # Set up the connection parameters
-    conn = pymysql.connect(
-    	host='motivy-redshift-cluster.cxrt7addrmk7.us-east-1.redshift.amazonaws.com',
-	user='rdsamin',
-	password='A0so%33r7Jf6',
-	database='dev',
-	cursorclass=pymysql.cursors.DictCursor
-    )
+    print(f'Create Conexion')
+    conn = get_redshift_con()
+    print(f'Conexion Succeded')
 
+   
+    # Set up the cursor and excecute query
+    print(f'Create Cursor Query')
+    cur = conn.cursor()
+    query = 'SELECT * FROM exec_time e'
+    print(query)	
+
+    print(f'Excecute Query')
+    cur.execute(query)
+    print(cur.fetchall())
+
+    print(f'Committing changes')
+    conn.commit()
+
+    print(f'Closing Connection')
+    cur.close()
     conn.close()
 
-    message = "Hello, world!"
-    return message
+    message = "Eof"
+    print(message)
+
+    return "Exited with status code 200"
 
 #event = {
 #   "key1":"value1"
