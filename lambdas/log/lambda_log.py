2f632fcba44c49ac20459d708b30af51916dd9b8
DanielR-spec <daniel_reyes@javeriana.edu.co>
2023-05-04 10:07:31 -0500

    v35

--- lambdas/log/lambda_log.py
+++ lambdas/log/lambda_log.py
@@ -1,30 +1,8 @@
 #Import library
 
-import boto3 as b3
-import json as js
 import psycopg2
-import sqlalchemy
-from sqlalchemy import create_engine
+import pymysql
 
-
-'''
-client = b3.client('lambda', region_name='us-east-1')
-
-
-event = {
-	"id1":"2023-20-23"
-}
-
-response = client.invoke(
-    FunctionName='motivy-analytics-lambdas-LogLambda-ebhzDjXd2E9M',
-    Payload=js.dumps(event)
-)
-
-result = js.loads(response['Payload'].read())
-print(result)
-
-print("End of excecution")
-'''
 def lambda_handler(event, contect):
     # TODO implement
     '''
@@ -32,40 +10,6 @@ def lambda_handler(event, contect):
 	import the boto library, contect to the rsd wharehouse 
 	with the corresponding aws access keys/secrets and variables to finally test
 	the conexion executing a query
-    '''
-    '''
-    # Set up the connection parameters
-     db_username = 'your_db_username'
-     db_password = 'your_db_password'
-     db_host = 'your_redshift_cluster_endpoint'
-     db_port = 5439
-     db_name = 'your_database_name'
-
-     # Construct the connection string
-     conn_string = f'postgresql://{db_username}:{db_password}@{db_host}:{db_port}/{db_name}'
-
-     # Create the SQLAlchemy engine
-     engine = create_engine(conn_string)
-
-     # Execute a sample query
-     with engine.connect() as conn:
-        result = conn.execute('SELECT COUNT(*) FROM your_table')
-        count = result.fetchone()[0]
-        print(f'Total rows in table: {count}')
-   
-   
- 
-    # Construct the connection string
-    cur = conn.cursor()
-    cur.execute("select * from exec_time et")
-    rows = cur.fetchall()
-
-    # Execute a sample query
-    cur.execute("INSERT INTO mytable (id, name, age) VALUES (%s, %s, %s)", (1, 'John Doe', 30))
-    with open('data.csv', 'r') as f:
-	 cur.copy_expert('COPY mytable FROM STDIN WITH CSV HEADER', f)
-    conn.commit()
-    print(rows)
     '''
 
      # Set up the connection parameters
