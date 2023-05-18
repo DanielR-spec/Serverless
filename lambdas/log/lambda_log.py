b1ab6f697ff09437e7e26afcb7f11e81762820b8
DanielR-spec <daniel_reyes@javeriana.edu.co>
2023-05-17 23:50:32 -0500

    v16 auto add id

--- lambdas/log/lambda_log.py
+++ lambdas/log/lambda_log.py
@@ -61,19 +61,8 @@ def lambda_handler(event, context):
             cursor: redshift_connector.Cursor = conn.cursor()
             table = "execution_time_logs_dev"
 
-        # Get unique id 
-            get_id = f"SELECT MAX(id) from {table}"
-            cursor.execute(get_id)
-            max_id = cursor.fetchone()[0]
-            conn.commit()
-            if max_id is None:
-            	max_id = 1
-            else:
-            	max_id = max_id + 1
-
         # Construct the json instert object
             insert = {
-                "id":max_id,
                 "user_id":body["user_id"],
                 "user_email":body["user_email"],
                 "first_name":body["user_name"],
@@ -97,7 +86,7 @@ def lambda_handler(event, context):
             
 
         # Construct the SQL statement to insert data into the Redshift table
-            insert_sql = f"INSERT INTO {table} (id,user_id,user_email,first_name,last_name,organization_id,organization_name,request_type,request_path,request_timestamp,response_timestamp,duration,response_status,device_type,country_id,time_zone,error) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+            insert_sql = f"INSERT INTO {table} (user_id,user_email,first_name,last_name,organization_id,organization_name,request_type,request_path,request_timestamp,response_timestamp,duration,response_status,device_type,country_id,time_zone,error) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
 
         # Execute the insert statement to load data into the Redshift table
             cursor.execute(insert_sql, json_list)
@@ -143,22 +132,21 @@ event = {
   },
   'body': {
     'user_id': 34,
-    'user_email': 'juan@motivy.co',
-    'user_name': 'Juan avalatina',
+    'user_email': 'juan123@motivy.co',
+    'user_name': 'Juan',
     'user_last': 'Brown',
     'org_id': 2,
-    'org_name': 'PBD',
+    'org_name': 'PDB',
     'request_type': 'POST',
     'request_path': '/login',
     'request_timestamp': '2023-05-16T06:45:37.141Z',
     'response_timestamp': '2023-05-16T06:45:37.332Z',
-    'duration': 101,
+    'duration': 73,
     'response_status': True,
-    'device_type': 'Mobil',
+    'device_type': 'Desktop',
     'user_country_id': 2,
     'user_time_zone': 'America/Los_Angeles',
     'error': 'Exited with status code 200'
   }
 }
-lambda_handler(event,None)
-'''
+lambda_handler(event,None)'''
